b756ce86eac39cc637c337977aaaaaff6a5f1485
Anabella Rizzi <anabeeella.r@gmail.com>
2022-04-07 10:07:40 -0300

    003_"Marquee"_AddingJs

--- main.js
+++ main.js
@@ -19,3 +19,92 @@ const closeDrawer = ()=> {
 
 closeDrawer();
 openDrawer();*/
+
+
+
+/*Marquee*/
+
+var Marquee = function (element, defaults) {
+  var elem = element,
+    options = (defaults === undefined) ? {} : defaults,
+    continuous = options.continuous || true,	// once or continuous
+    direction = options.direction || 'ltr', 	// ltr or rtl
+    loops = options.loops || -1,
+    speed = options.speed || 0.5,
+    milestone = 0,
+    marqueeElem = null,
+    elemWidth = null,
+    self = this,
+    ltrCond = 0,
+    loopCnt = 0,
+    start = 0,
+    opacity = options.opacity || 1.0,
+    process = null,
+    constructor = function (elem) {
+
+      // Build html
+      var elemHTML = elem.innerHTML;
+      var elemNode = elem.childNodes[1] || elem;
+      elemWidth = elemNode.offsetWidth;
+      marqueeElem = '<div>' + elemHTML + '</div>';
+      elem.innerHTML = marqueeElem;
+      marqueeElem = elem.getElementsByTagName('div')[0];
+      elem.style.overflow = 'hidden';
+      marqueeElem.style.whiteSpace = 'nowrap';
+      marqueeElem.style.position = 'relative';
+      marqueeElem.style.opacity = opacity;
+
+      if (continuous) {
+        marqueeElem.innerHTML += elemHTML;
+        marqueeElem.style.width = '100%';
+
+        if (direction === 'ltr') {
+          start = -elemWidth;
+        }
+      } else {
+        ltrCond = elem.offsetWidth;
+
+        if (direction === 'rtl') {
+          milestone = ltrCond;
+        }
+      }
+
+      if (direction === 'ltr') {
+        milestone = -elemWidth;
+      } else if (direction === 'rtl') {
+        speed = -speed;
+      }
+
+      self.start();
+
+      return marqueeElem;
+    }
+
+  this.start = function () {
+    process = window.setInterval(function () {
+      self.play();
+    });
+  };
+
+  this.play = function () {
+    // beginning
+    marqueeElem.style.left = start + 'px';
+    start = start + speed;
+
+    if (start > ltrCond || start < -elemWidth) {
+      start = milestone;
+      loopCnt++;
+
+      if (loops !== -1 && loopCnt >= loops) {
+        marqueeElem.style.left = 0;
+      }
+    }
+  }
+
+  this.end = function () {
+    window.clearInterval(process);
+  }
+
+  // Init plugin
+  marqueeElem = constructor(elem);
+}
