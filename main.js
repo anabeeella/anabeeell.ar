37336339362322438a6e574b91959002022f05fa
Anabella Rizzi <anabeeella.r@gmail.com>
2022-04-06 21:33:14 -0300

    001_first-commit_"TheBegining"

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+const drawer = document.querySelector('.list-nav');
+const openDrawerElement = document.querySelector('#open-nav');
+const closeDrawerElement = document.querySelectorAll('.close-drawer');
+
+const openDrawer = ()=>{
+  openDrawerElement.addEventListener('click', ()=> {
+    drawer.style.setProperty("display", "flex")
+  })
+}
+
+const closeDrawer = ()=> {
+  closeDrawerElement.forEach(element => {
+    element.addEventListener('click', ()=> {
+      drawer.style.setProperty("display", "none")
+    })
+  })
+}
+
+closeDrawer();
+openDrawer();*/
